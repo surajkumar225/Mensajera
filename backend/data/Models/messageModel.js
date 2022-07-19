b6b2ed01e189c9ea1f0706151191e212c3c646b0
Suraj kumar <96677059+surajkumar225@users.noreply.github.com>
2022-07-19 22:51:29 +0530

    Chat App

---
+++ backend/data/Models/messageModel.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const messageModel = mongoose.Schema(
+  {
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    content: { type: String, trim: true },
+    chat: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Message = mongoose.model("User", messageModel);
+
+module.exports = Message;

11840d2c68222c0696b453099dfc53a3151b9a42
Suraj kumar <96677059+surajkumar225@users.noreply.github.com>
2022-07-20 15:42:52 +0530

    Chat App

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -8,19 +8,19 @@ import {
   Tabs,
   Text,
 } from "@chakra-ui/react";
-import { useEffect } from "react";
-import { useHistory } from "react-router";
-import Login from "../components/Authentication/Login";
-import Signup from "../components/Authentication/Signup";
+// import { useEffect } from "react";
+// import { useHistory } from "react-router";
+import Login from "../Components/Authentication/Login";
+import Signup from "../Components/Authentication/Signup";
 
 function Homepage() {
-  const history = useHistory();
+  // const history = useHistory();
 
-  useEffect(() => {
-    const user = JSON.parse(localStorage.getItem("userInfo"));
+  // useEffect(() => {
+  //   const user = JSON.parse(localStorage.getItem("userInfo"));
 
-    if (user) history.push("/chats");
-  }, [history]);
+  //   if (user) history.push("/chats");
+  // }, [history]);
 
   return (
     <Container maxW="xl" centerContent>
@@ -30,19 +30,24 @@ function Homepage() {
         p={3}
         bg="white"
         w="100%"
-        m="40px 0 15px 0"
+        m="40px 0px 15px 0px"
         borderRadius="lg"
         borderWidth="1px"
       >
-        <Text fontSize="4xl" fontFamily="Work sans">
-          Talk-A-Tive
+        <Text
+          fontSize="4xl"
+          justifyContent="center"
+          alignItems="center"
+          fontFamily="Work sans"
+        >
+          Parley
         </Text>
       </Box>
       <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
         <Tabs isFitted variant="soft-rounded">
           <TabList mb="1em">
-            <Tab>Login</Tab>
-            <Tab>Sign Up</Tab>
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign Up</Tab>
           </TabList>
           <TabPanels>
             <TabPanel>

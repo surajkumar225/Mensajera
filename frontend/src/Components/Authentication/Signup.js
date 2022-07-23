585407cd95264d1a681ac710ed16698acc5c60d1
Suraj kumar <96677059+surajkumar225@users.noreply.github.com>
2022-07-23 21:02:20 +0530

    Chat App

--- frontend/src/Components/Authentication/Signup.js
+++ frontend/src/Components/Authentication/Signup.js
@@ -84,50 +84,50 @@ const Signup = () => {
     }
   };
 
-  const postDetails = (pics) => {
-    setPicLoading(true);
-    if (pics === undefined) {
-      toast({
-        title: "Please Select an Image!",
-        status: "warning",
-        duration: 5000,
-        isClosable: true,
-        position: "bottom",
-      });
-      return;
-    }
-    console.log(pics);
-    if (pics.type === "image/jpeg" || pics.type === "image/png") {
-      const data = new FormData();
-      data.append("file", pics);
-      data.append("upload_preset", "chat-app");
-      data.append("cloud_name", "piyushproj");
-      fetch("https://api.cloudinary.com/v1_1/piyushproj/image/upload", {
-        method: "post",
-        body: data,
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          setPic(data.url.toString());
-          console.log(data.url.toString());
-          setPicLoading(false);
-        })
-        .catch((err) => {
-          console.log(err);
-          setPicLoading(false);
-        });
-    } else {
-      toast({
-        title: "Please Select an Image!",
-        status: "warning",
-        duration: 5000,
-        isClosable: true,
-        position: "bottom",
-      });
-      setPicLoading(false);
-      return;
-    }
-  };
+  // const postDetails = (pics) => {
+  //   setPicLoading(true);
+  //   if (pics === undefined) {
+  //     toast({
+  //       title: "Please Select an Image!",
+  //       status: "warning",
+  //       duration: 5000,
+  //       isClosable: true,
+  //       position: "bottom",
+  //     });
+  //     return;
+  //   }
+  //   console.log(pics);
+  //   if (pics.type === "image/jpeg" || pics.type === "image/png") {
+  //     const data = new FormData();
+  //     data.append("file", pics);
+  //     data.append("upload_preset", "chat-app");
+  //     data.append("cloud_name", "piyushproj");
+  //     fetch("https://api.cloudinary.com/v1_1/piyushproj/image/upload", {
+  //       method: "post",
+  //       body: data,
+  //     })
+  //       .then((res) => res.json())
+  //       .then((data) => {
+  //         setPic(data.url.toString());
+  //         console.log(data.url.toString());
+  //         setPicLoading(false);
+  //       })
+  //       .catch((err) => {
+  //         console.log(err);
+  //         setPicLoading(false);
+  //       });
+  //   } else {
+  //     toast({
+  //       title: "Please Select an Image!",
+  //       status: "warning",
+  //       duration: 5000,
+  //       isClosable: true,
+  //       position: "bottom",
+  //     });
+  //     setPicLoading(false);
+  //     return;
+  //   }
+  // };
 
   return (
     <VStack className="ChatForm" spacing="5px">
@@ -176,7 +176,7 @@ const Signup = () => {
           </InputRightElement>
         </InputGroup>
       </FormControl>
-      <FormControl id="pic">
+      {/* <FormControl id="pic">
         <FormLabel>Upload your Picture</FormLabel>
         <Input
           type="file"
@@ -184,9 +184,9 @@ const Signup = () => {
           accept="image/*"
           onChange={(e) => postDetails(e.target.files[0])}
         />
-      </FormControl>
+      </FormControl> */}
       <Button
-        colorScheme="blue"
+        colorScheme="red"
         width="100%"
         style={{ marginTop: 15 }}
         onClick={submitHandler}

dd0f679b6545251dcd69905d472b6d32dbf66bcd
Suraj kumar <96677059+surajkumar225@users.noreply.github.com>
2022-07-23 22:14:50 +0530

    Chat App

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data.js");
+const connectDB = require("./Config/db.js");
 
 const app = express();
 dotenv.config();
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("API is running successfully");
